507aafc759dbda9689732a1d9d80402644663ec6
Jose Soarez <jsoarez@MacBook-Pro.local>
2019-04-18 23:11:41 -0300

    First set up of xcode enviroment for the project

---
+++ Market Place/Market Place/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Market Place
+//
+//  Created by Jose Soarez on 4/18/19.
+//  Copyright © 2019 Jose Soarez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
